3c893427b9dd0c5b859fd0bfc841db9ad45fd936
Iruiasaro <brunettirosario@gmail.com>
2021-04-14 16:56:26 +0200

    condizione numero da 1 a 5

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -4,4 +4,9 @@ var pariDispari = prompt("Pari o Dispari?");
 //scegliere un numero da 1 a 5
 var numeroScelto = parseInt(prompt("Da 1 a 5 quale numero scegli?"));
 
-//
+
+//l'utente deve necessariamente inserire un numero da 1 a 5
+if (Number.isNaN(numeroScelto) || numeroScelto < 1 || numeroScelto > 5) {
+    alert("ATTENZIONE, DEVI INSERIRE UN NUMERO DA 1 A 5!")
+}
+
