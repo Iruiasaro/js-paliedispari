836d24318997ebe62bfb6f63adc48f8c56b68428
Iruiasaro <brunettirosario@gmail.com>
2021-04-14 15:38:21 +0200

    inizializzed impostazione esercio pariDispari

---
+++ pari e dispari/script.js
@@ -0,0 +1,7 @@
+//scegli se essere pari o dispari
+var pariDispari = prompt("Pari o Dispari?");
+
+//scegliere un numero da 1 a 5
+var numeroScelto = parseInt(prompt("Da 1 a 5 quale numero scegli?"));
+
+//
